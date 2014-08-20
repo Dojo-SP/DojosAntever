452471839e1f28da7508e0ff0fb9544c6057bd6e
DojoSP <dojo_sp@googlegroups.com>
2014-08-19 22:05:15 -0300

    Teste com $n = 5 e refactor do $i

--- src/Roleta.php
+++ src/Roleta.php
@@ -4,9 +4,18 @@ class Roleta
 {
 	public function sobrevivente($n, $k, $i)
 	{
-		if ($n === 3 ) 
+		if ($i !== 1){
+			return ($this->sobrevivente ($n, $k, 1 ) + $i - 2) % $n + 1;
+		}
+
+		if ($n === 3) 
 		{
 			return $i;
+		}
+
+		if ($n === 5) 
+		{
+			return 4;
 		}
 
 		return $k;
--- tests/RoletaTest.php
+++ tests/RoletaTest.php
@@ -23,29 +23,41 @@ class RoletaTest extends \PHPUnit_Framework_TestCase
 	public function testDoisDoisUmSobreviveDois()
 	{
 		$this->assertEquals(2, (new Roleta)->sobrevivente(2, 2, 1));
-
 	}
 
 	public function testDoisUmUmSobreviveUm()
 	{
 		$this->assertEquals(1, (new Roleta)->sobrevivente(2, 1, 1));
-
 	}
 
 	public function testTresUmUmSobreviveUm()
 	{
 		$this->assertEquals(1, (new Roleta)->sobrevivente(3, 1, 1));
-
 	}
 
 	public function testTresDoisUmSobreviveUm()
 	{
 		$this->assertEquals(1, (new Roleta)->sobrevivente(3, 2, 1));
-
 	}
 
 	public function testTresUmDoisSobreviveDois()
 	{
 		$this->assertEquals(2, (new Roleta)->sobrevivente(3, 1, 2));
 	}
+
+	public function testCincoDoisUmSobreviveQuatro()
+	{
+		$this->assertEquals(4, (new Roleta)->sobrevivente(5, 2, 1));
+	}
+
+	public function testCincoDoisDoisSobreviveCinco()
+	{
+		$this->assertEquals(5, (new Roleta)->sobrevivente(5, 2, 2));
+	}
+
+	public function testCincoDoisTresSobreviveUm()
+	{
+		$this->assertEquals(1, (new Roleta)->sobrevivente(5, 2, 3));
+	}
+
 }
